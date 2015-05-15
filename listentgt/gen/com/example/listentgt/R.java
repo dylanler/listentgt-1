449d2a87af0b84c2add12653be2f3911df88ad6d
yaodaniel <Daniel988@ucla.edu>
2015-05-14 19:28:38 -0700

    This will show all songs on user's phone

--- listentgt/gen/com/example/listentgt/R.java
+++ listentgt/gen/com/example/listentgt/R.java
@@ -22,17 +22,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int end=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int play=0x7f020002;
+        public static final int rand=0x7f020003;
     }
     public static final class id {
-        public static final int Connect=0x7f080002;
+        public static final int Connect=0x7f080003;
         public static final int PD=0x7f080000;
-        public static final int action_settings=0x7f080003;
-        public static final int listView=0x7f080001;
+        public static final int action_end=0x7f080007;
+        public static final int action_shuffle=0x7f080006;
+        public static final int listView=0x7f080002;
+        public static final int song_artist=0x7f080005;
+        public static final int song_list=0x7f080001;
+        public static final int song_title=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int bluetooth_connect=0x7f030001;
+        public static final int song=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
